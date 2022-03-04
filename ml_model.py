8a00f6f9b7cbba218bbcf49f3f9ceb7112764736
amitbhattacharya4 <amitbhattacharyadit@gmail.com>
2022-03-04 15:35:05 +0800

    adding variousl models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 print ("my ml model")
+
+def model1():
+    print("adding code for model1")
+
+def model2():
+    print("adding code for model1")
+
+
+def model2():
+    print("adding code for model1")
+
